23a02006c6d1dad595ab03c4953d1576d3b1c998
vslobo <antilamer1709@gmail.com>
2020-05-05 17:21:43 +0300

    GRASP, Creator clarification

--- src/ua/vslobo/examples/grasp/ExpenseServiceImpl.java
+++ src/ua/vslobo/examples/grasp/ExpenseServiceImpl.java
@@ -3,6 +3,7 @@ package ua.vslobo.examples.grasp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ua.vslobo.examples.grasp.mock.AnotherService;
 import ua.vslobo.examples.layered.architecture.mock.ExpenseDTO;
 import ua.vslobo.examples.layered.architecture.mock.ExpenseEntity;
 import ua.vslobo.examples.layered.architecture.mock.ExpenseService;
@@ -11,12 +12,14 @@ import ua.vslobo.examples.layered.architecture.mock.ExpenseService;
 public class ExpenseServiceImpl implements ExpenseService {
 
     @Autowired
+    private AnotherService anotherService;
     // services and infrastructure
 
 
     @Override
     @Transactional
     public void createExpense(ExpenseDTO expenseDTO) {
+        // here we create an object
         ExpenseEntity expenseEntity = new ExpenseEntity();
         businessLogicMethod(expenseEntity);
     }
@@ -29,6 +32,7 @@ public class ExpenseServiceImpl implements ExpenseService {
 
     private void anotherBusinessLogicMethod(ExpenseEntity expenseEntity) {
         // here we use expenseEntity
+        anotherService.processExpenseEntity(expenseEntity); // anotherService should create and return the ExpenseEntity
     }
 
 }
---
+++ src/ua/vslobo/examples/grasp/mock/AnotherService.java
@@ -0,0 +1,10 @@
+package ua.vslobo.examples.grasp.mock;
+
+import ua.vslobo.examples.layered.architecture.mock.ExpenseEntity;
+
+public class AnotherService {
+
+    public void processExpenseEntity(ExpenseEntity expenseEntity) {
+    }
+
+}
